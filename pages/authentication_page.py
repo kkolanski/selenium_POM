c015256cbc0a534d4e5f43977bd73436f92c5a5e
Karol Kolański <gralu86@gmail.com>
2026-03-14 15:30:07 +0100

    Powoli wygaszamy piec

--- pages/authentication_page.py
+++ pages/authentication_page.py
@@ -1,11 +1,13 @@
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
+from pages.create_account_page import CreateAccountPage
 
 class Locators:
     """
     Authentication Page locators
     """
     CREATE_ACCOUNT_EMAIL = (By.ID, "email_create")
+    CREATE_ACCOUNT_BTN = (By.ID, "SubmitCreate")
 
 class AuthenticationPage(BasePage):
     """
@@ -18,3 +20,11 @@ class AuthenticationPage(BasePage):
         :return:
         """
         self.driver.find_element(*Locators.CREATE_ACCOUNT_EMAIL).send_keys(email)
+
+    def click_create_account(self):
+        """
+        Clicks Create Account
+        :return: CreateAccountPage Object
+        """
+        self.driver.find_element(*Locators.CREATE_ACCOUNT_BTN).click()
+        return CreateAccountPage(self.driver)
