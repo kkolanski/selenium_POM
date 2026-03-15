7b055fa7621c607ad8b7989c1855de3531986cfd
Karol Kolański <kkolanski.edu@gmail.com>
2026-03-15 14:32:57 +0100

    Finished test no last name

--- tests/registration_test.py
+++ tests/registration_test.py
@@ -19,7 +19,9 @@ class RegistrationTest(BaseTest):
         self.create_account_page.enter_password(self.data.PASSWORD)
         self.create_account_page.select_date_of_birth(self.data.DATE_OF_BIRTH)
         self.create_account_page.click_register_button()
+        expected_number_of_errors_message = "There is 1 error"
+        actual_number_of_errors_message = self.create_account_page.get_number_of_errors_message()
+        self.assertEqual(expected_number_of_errors_message, actual_number_of_errors_message)
         visible_errors = self.create_account_page.get_visible_errors()
         expected_errors = ["lastname is required."]
         self.assertCountEqual(expected_errors, visible_errors)
-        sleep(2)
