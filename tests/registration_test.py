19bf9caa19a7aab5a578df7c3a5a4b025919d61f
Karol Kolański <kkolanski.edu@gmail.com>
2026-03-17 18:36:16 +0100

    Added initial DDT

--- tests/registration_test.py
+++ tests/registration_test.py
@@ -1,7 +1,10 @@
+import test_data.registration_data
 from tests.base_test import BaseTest
 from time import sleep
 from test_data.registration_data import RegistrationDataGenerator
+from ddt import ddt, data, unpack
 
+@ddt
 class RegistrationTest(BaseTest):
     def setUp(self):
         super().setUp()
@@ -11,12 +14,13 @@ class RegistrationTest(BaseTest):
         self.authentication_page.enter_create_account_email(self.data.EMAIL)
         self.create_account_page = self.authentication_page.click_create_account()
 
-
-    def testNoLastname(self):
+    @data(*test_data.registration_data.get_csv_data("test_data/registration.csv"))
+    @unpack
+    def testNoLastname(self, gender, firstname, lastname, email, password, day, month, year):
         self.create_account_page.choose_gender(self.data.GENDER)
-        self.create_account_page.enter_first_name(self.data.FIRST_NAME)
+        self.create_account_page.enter_first_name(firstname)
         self.assertEqual(self.data.EMAIL, self.create_account_page.get_entered_email())
-        self.create_account_page.enter_password(self.data.PASSWORD)
+        self.create_account_page.enter_password(password)
         self.create_account_page.select_date_of_birth(self.data.DATE_OF_BIRTH)
         self.create_account_page.click_register_button()
         expected_number_of_errors_message = "There is 1 error"
